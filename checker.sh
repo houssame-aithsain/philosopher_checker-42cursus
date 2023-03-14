12ddbf189c50b183373f744fe9a67f229e808b4a
Houssame Ait hsain <88860095+houssame-aithsain@users.noreply.github.com>
2023-03-14 17:11:22 +0100

    Update checker.sh

--- checker.sh
+++ checker.sh
@@ -11,14 +11,14 @@
 # **************************************************************************** #
 
 handle_sigint() {
-	kill -9 $(ps | grep ./philosopher | awk 'NR==1 {print $1}') >/dev/null 2>&1
+	kill -9 $(ps | grep ./philo | awk 'NR==1 {print $1}') >/dev/null 2>&1
 	echo "
 	Wait..."
 }
 make
 touch time.txt time1.txt
 cd ..;make re;cd philosopher_checker-42cursus;
-cp ../philosopher ./
+cp ../philo ./
 clear
 echo "number_of_philosophers"
 read -r number_of_philosophers
@@ -32,7 +32,7 @@ trap handle_sigint SIGINT
 echo "Your program is now running...."
 echo "No philosopher has died yet ?"
 echo "Please wait for 0 < 5-seconds < 6 before pressing CTRL+C to stop the program and begin testing."
-./philosopher  $number_of_philosophers $time_to_die $time_to_eat $time_to_sleep > time.txt
+./philo  $number_of_philosophers $time_to_die $time_to_eat $time_to_sleep > time.txt
 cat time.txt | awk '{print $1}' | tr '\n' ' ' > time1.txt
 ./checker "$(cat time1.txt)"
 echo "
